e688b0a1cd3574620bedaab1fd113930dedfce10
jwd25 <jwinterday@mail.ru>
2021-09-17 12:25:45 +0300

    draft

--- scripts/app.sh
+++ scripts/app.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 ROOT=$PWD
-APP_TYPE=$1
+APP_TYPE=$1 # debug mock production_services
 
 source scripts/constants.sh
+
+
+
+if [[ "$APP_TYPE" == "" ]]; then
+    APP_TYPE="mock";
+fi
 
 function checking_flutter_version {
   cd $(dirname $(which flutter)) && cd ..
@@ -18,8 +24,25 @@ function checking_flutter_version {
 }
 
 function creating_links {
-    ln -sfn $ROOT/app_bundle/google_services/GoogleService-Info.plist $ROOT/ios/Runner/GoogleService-Info.plist # ios
-    ln -sfn $ROOT/app_bundle/google_services/google-services.json $ROOT/android/app/google-services.json #android
+    # debug, mock
+    if [[ "$APP_TYPE" == "debug" ]] || [[ "$APP_TYPE" == "mock" ]]; then
+        ln -sfn "$ROOT/app_bundle/google_services/GoogleService-Info.plist" "$ROOT/ios/Runner/" # ios
+        ln -sfn "$ROOT/app_bundle/google_services/google-services.json" "$ROOT/android/app/" # android
+    fi
+
+    # production_services
+    if [[ "$APP_TYPE" == "production_services" ]]; then
+        # folder under with production google service settings
+        cd ../env/all_school_info/
+        ENV_PATH=$(pwd)
+
+        ln -sfn "$ENV_PATH/GoogleService-Info.plist" "$ROOT/ios/Runner/GoogleService-Info.plist" # ios
+        ln -sfn "$ENV_PATH/google-services.json" "$ROOT/android/app/google-services.json"  # android
+
+        cd $ROOT
+
+        pwd
+    fi
 }
 
 # call functions
